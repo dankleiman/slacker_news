78f4685811f30da952368ec4598b19e73c6d8853
Dan Kleiman <info@dankleiman.com>
2014-05-22 14:44:41 -0400

    You can add articles and view all submitted articles.

--- server.rb
+++ server.rb
@@ -1,3 +1,30 @@
 require 'sinatra'
 require 'csv'
 require 'pry'
+
+def get_articles
+  articles = []
+  CSV.foreach('articles.csv', headers: true, header_converters: :symbol) do |article|
+      articles << article.to_hash
+  end
+  articles
+end
+
+get '/' do
+  @articles = get_articles
+  erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/' do
+  title = params[:title]
+  url = params[:url]
+  description = params[:description]
+
+  CSV.open("articles.csv", "a") { |csv| csv << [title, url, description]  }
+  redirect "/"
+end
+
