630dc7441007897a12e0379b5abb1a21863da3b7
Dan Kleiman <info@dankleiman.com>
2014-05-25 14:24:52 -0400

    Updated form validation to ensure descriptions and valid, non-duplicate urls, and updated error message styling.

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'csv'
 require 'pry'
+require 'uri'
 
 def get_articles
   articles = []
@@ -10,21 +11,60 @@ def get_articles
   articles
 end
 
+def form_errors(title, url, description)
+  errors = []
+  if title == ""
+      errors << "Please enter a title."
+  end
+  #validate description
+  if description == ""
+      errors << "Please enter a description."
+  end
+  if description.length < 20
+    errors << "Please enter a description of at least 20 characters."
+  end
+  #validate url
+  if (url =~ URI::regexp) != 0
+      errors << "Please enter a valid url."
+  end
+  existing_articles = get_articles
+  # binding.pry
+  existing_articles.each do |old_article|
+    if old_article[:url] == (url)
+      errors << "This article has already been submitted. Please submit something else."
+    end
+  end
+
+  errors
+end
+
 get '/' do
   @articles = get_articles
   erb :index
 end
 
 get '/new' do
+  @errors = []
   erb :new
 end
 
-post '/' do
+post '/new' do
   title = params[:title]
   url = params[:url]
   description = params[:description]
+  @errors = []
+  #validate form entries
+  @errors = form_errors(title, url, description)
+  # binding.pry
+  if @errors.empty? == true
+    # binding.pry
+      CSV.open("articles.csv", "a") { |csv| csv << [title, url, description]  }
+      redirect '/'
+  else
+    erb :new
+  end
 
-  CSV.open("articles.csv", "a") { |csv| csv << [title, url, description]  }
-  redirect "/"
 end
 
+ # query = params.map{|key, value| "#{key}=#{value}"}.join("&")
+ #      redirect "/new?#{query}"
